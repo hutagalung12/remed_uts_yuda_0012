c1e5c8fcbba50a29182e6b36ac28a3f8eab6c47d
hutagalung12 <email@yudaroytoba@gmail.com>
2026-05-13 18:40:32 +0700

    Create dan Read

--- resources/views/kendaraan/index.blade.php
+++ resources/views/kendaraan/index.blade.php
@@ -101,21 +101,7 @@
 
                         <td class="d-flex gap-2">
 
-                            <a href="/kendaraan/{{ $item->id }}/edit"
-                                class="btn btn-edit btn-sm">
-                                Edit
-                            </a>
-
-                            <form action="/kendaraan/{{ $item->id }}" method="POST"
-                                onsubmit="return confirm('Hapus kendaraan dari antrean?')">
-
-                                @csrf
-                                @method('DELETE')
-
-                                <button type="submit"
-                                    class="btn btn-delete btn-sm">
-                                    Hapus
-                                </button>
+                           
 
                             </form>
 
